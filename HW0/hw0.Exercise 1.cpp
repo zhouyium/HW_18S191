02768e28c71986f0646d64fd332824e6f864bdd5
周毅 <yc07444@um.edu.mo>
2020-11-27 13:37:38 +0800

    hw0 exercise 1，求 x 的平方跟，使用 c++ 实现。

---
+++ HW0/hw0.Exercise 1.cpp
@@ -0,0 +1,50 @@
+//Homework 0: Getting up and running
+//Exercise 1 - Square root by Newton's method
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//如果提交到OJ，不要定义 __LOCAL
+#define __LOCAL
+
+const double EPS = 1e-8;
+
+double solve(double x) {
+    double ans=rand()%((long long)(ceil(x)))+0.1;
+    double t;
+    do {
+        t=x/ans;
+        ans = (t+ans)/2;
+    } while (abs(t-ans)>EPS);
+    return ans;
+}
+
+int main() {
+#ifndef __LOCAL
+    //这部分代码需要提交到OJ，本地调试不使用
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    //输入一个x
+    cout<<fixed<<setprecision(12)<<"Please input a number x, which is positive.the error margin is "<<EPS<<"\n";
+    double x;
+    cin>>x;
+
+    if (x<=0) {
+        cout<<"Input error!\n";
+        return 0;
+    }
+
+    srand(time(0));
+    cout<<"Using iteration the square root of "<<x<<" is ";
+    cout<<solve(x)<<"\n";
+    cout<<"Using function sqrt the square root of "<<x<<" is ";
+    cout<<sqrt(x)<<"\n";
+
+#ifdef __LOCAL
+    //这部分代码不需要提交到OJ，本地调试使用
+    system("pause");
+#endif
+    return 0;
+}
